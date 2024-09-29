c306f1b2a0076d953453768cb4710895873cff6c
Divine-coder250 <lincadivine@gmail.com>
2024-09-29 12:50:18 +0200

    images changed

--- front/src/components/ImageCarouselK.jsx
+++ front/src/components/ImageCarouselK.jsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const ImageCarousel = () => {
-  // Array of images
+  // Array of 3 images
   const images = [
     "/images/image1.jpg",
     "/images/image2.jpg",
-    "/images/image3.jpg",
-    "/images/image4.jpg",
-    "/images/image5.jpg", // Add as many images as you want
+    "/images/image3.jpg",  
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -17,14 +15,14 @@ const ImageCarousel = () => {
   // Navigate to the next image
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1 // Decrement for right-to-left
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1 // Increment to show next image
     );
   };
 
   // Navigate to the previous image
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1 // Increment for right-to-left
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1 // Decrement to show previous image
     );
   };
 
@@ -38,19 +36,8 @@ const ImageCarousel = () => {
     }
   }, [currentIndex, isPaused]);
 
-  // Calculate the indices for the side images
-  const nextIndex = (currentIndex + 1) % images.length; // Next image
-  const prevIndex = (currentIndex - 1 + images.length) % images.length; // Previous image
-
   return (
     <div id="Urugendo" className="flex items-center justify-center space-x-4">
-      {/* Previous image */}
-      <img
-        src={images[prevIndex]}
-        alt="Previous"
-        className="h-96 w-72 object-cover rounded-lg opacity-70 border-8 border-gray-500/50" // Increased image size (h-96, w-72)
-      />
-
       {/* Chevron left */}
       <FaChevronLeft
         onClick={prevSlide}
@@ -70,13 +57,6 @@ const ImageCarousel = () => {
       <FaChevronRight
         onClick={nextSlide}
         className="cursor-pointer text-5xl text-black"
-      />
-
-      {/* Next image */}
-      <img
-        src={images[nextIndex]}
-        alt="Next"
-        className="h-96 w-72 object-cover rounded-lg opacity-70 border-8 border-gray-500/50" // Increased image size (h-96, w-72)
       />
     </div>
   );
