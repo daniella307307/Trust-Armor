154f287df72b2fde00128bfe9c6ce116dc1f9741
daniella307307 <daniellaganza30@gmail.com>
2024-09-29 12:59:38 +0200

    Changed How component

--- front/src/components/How.jsx
+++ front/src/components/How.jsx
@@ -7,7 +7,7 @@ const How = () => {
       <div className="max-w-2xl mx-auto">
         <h1 className="text-4xl text-black font-bold">How It Works!</h1>
         <p className="mt-4 text-gray-500 text-lg">
-          Say Goodbye To Juggling Multiple Apps And Platforms. With Our Intuitive Interface And Powerful Features, Financial Clarity And Control Are Now At Your Fingertips.
+           TrustArmor is the automatic fraudulent link and Image Source Detection Browser Extension. It is a comprehensive security tool that goes beyond scanning links to also verify the authenticity of images received online. By leveraging advanced algorithms and techniques, this extension aims to automatically detect potentially fraudulent links and verify the sources of images, enhancing user awareness and safeguarding against various online threats.
         </p>
       </div>
     </section>
